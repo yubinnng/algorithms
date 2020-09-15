f9ea0764679141dbcaee12bc30031b58bfed3163
Yubing <yubinnng@gmail.com>
2020-09-15 20:45:29 +0800

    Revert "Revert "developing""
    This reverts commit a02409609518e1e82ce1b0d3ff3e4cdf855367ae.

--- ai/wuziqi/board.py
+++ ai/wuziqi/board.py
@@ -113,6 +113,7 @@ class Board():
             'row': None,
             'col': None
         }
+
         for row, col in self.empty_indexes():
             temp_board = self.copy()
             temp_board.put(row, col, color)
