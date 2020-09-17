c000cea5c20ecdb0ea5c13d29e39fcbe052b1f84
Yubing <yubinnng@gmail.com>
2020-09-17 14:22:40 +0800

    developing

--- ai/wuziqi/board.py
+++ ai/wuziqi/board.py
@@ -4,29 +4,25 @@ import math
 from common import *
 
 class Board():
-    num = 0
+    leaf_num = 0
+
     def __init__(self, player_color: PieceColor, data=[]):
         assert isinstance(player_color, PieceColor)
         super().__init__()
         self.board_size = 15
-        self.steps = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        self.directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (1, 1), (-1,-1), (1, -1), (-1, 1)]
         self.player_color = player_color
         self.ai_color = player_color.reverse()
-        self.prev_step_player = None
-        self.prev_step_ai = None
-        self.history = []
+        self.candidates = {}
 
         if len(data):
             self.data = data
         else:
             self.data = np.zeros((self.board_size, self.board_size), dtype=int)
 
-        self.empty_idx = [(i, j) for i in range(self.board_size) for j in range(self.board_size)]
-
     def copy(self):
         new_board = Board(self.player_color, data=self.data.copy())
-        new_board.empty_idx = self.empty_idx.copy()
-        new_board.history = self.history.copy()
+        new_board.candidates = self.candidates.copy()
         return new_board
 
     def in_range(self, row, col) -> bool:
@@ -38,12 +34,10 @@ class Board():
         """
         return self.in_range(row, col) and self.data[row, col] == EMPTY_VALUE
 
-    def put(self, row, col, color, record_history=False):
+    def put(self, row, col, color, record=False):
         assert self.can_put(row, col)
         self.data[row, col] = color.value
-        self.empty_idx.remove((row, col))
-        if record_history:
-            self.history.append((row, col, color))
+        self.update_candidates(row, col, record)
 
     def cal_score(self, piece_list, color: PieceColor) -> int:
         if color == BLACK:
@@ -58,7 +52,9 @@ class Board():
             score_sum += count * v
         return score_sum
 
-    def evaluate(self, color) -> int:
+    def evaluate(self) -> int:
+        Board.leaf_num += 1
+
         line_list = []
         # heng
         line_list.extend(self.data.tolist())
@@ -94,16 +90,9 @@ class Board():
                 temp.append(self.data[j + i, -(j + 1)])
             line_list.append(temp)
 
-        if color == self.ai_color:
-            ai_ratio = 1
-            player_ratio = 0.1
-        else:
-            ai_ratio = 0.1
-            player_ratio = 1
-
         score_sum = 0
         for line in line_list:
-            score_sum += self.cal_score(line, self.ai_color)
+            score_sum += self.cal_score(line, self.ai_color) * 0.5
             score_sum -= self.cal_score(line, self.player_color)
         return score_sum
 
@@ -112,18 +101,24 @@ class Board():
         return math.sqrt((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2)
 
     def get_candidates(self) -> list:
-        results = {}
-        for emp_idx in self.empty_idx:
-            for hist_row, hist_col, _ in self.history:
-                dist = Board.l2_distance(emp_idx, (hist_row, hist_col))
-                if dist == 1 or dist == 2 or dist == 3 or abs(dist - 1.41) < 0.1 or abs(dist - 2.82) < 0.1 or abs(dist - 4.24) < 0.1:
-                    results[emp_idx] = dist
-        return sorted(results.keys(), key=lambda x: results[x])
+        return sorted(self.candidates.keys(), key=lambda x: self.candidates[x])
 
-    def max(self, depth, beta):
+    def update_candidates(self, new_row, new_col, extend):
+
+        if (new_row, new_col) in self.candidates:
+            self.candidates.pop((new_row, new_col))
+        if extend:
+            for i in range(1, 4):
+                for dir in self.directions:
+                    candi_idx = (new_row + i * dir[0], new_col + i * dir[1])
+                    if self.can_put(*candi_idx):
+                        self.candidates[candi_idx] = Board.l2_distance(candi_idx, (new_row, new_col))
+
+
+    def max(self, depth, alpha, beta):
         assert depth % 2 == 0
         max = {
-            'score': MIN,
+            'score': alpha,
             'row': None,
             'col': None
         }
@@ -132,7 +127,7 @@ class Board():
             temp_board = self.copy()
             temp_board.put(row, col, self.ai_color)
 
-            temp_score, _, _ = temp_board.min(depth - 1, max['score'])
+            temp_score, _, _ = temp_board.min(depth - 1, max['score'], beta)
 
             if temp_score > max['score']:
                 max['score'] = temp_score
@@ -143,9 +138,9 @@ class Board():
                     return temp_score, row, col
         return max['score'], max['row'], max['col']
 
-    def min(self, depth, alpha):
+    def min(self, depth, alpha, beta):
         min = {
-            'score': MAX,
+            'score': beta,
             'row': None,
             'col': None
         }
@@ -154,10 +149,9 @@ class Board():
             temp_board = self.copy()
             temp_board.put(row, col, self.player_color)
             if depth <= 1:
-                temp_score = temp_board.evaluate(self.player_color)
-                Board.num += 1
+                temp_score = temp_board.evaluate()
             else:
-                temp_score, _, _ = temp_board.max(depth - 1, min['score'])
+                temp_score, _, _ = temp_board.max(depth - 1, alpha, min['score'])
 
             if temp_score < min['score']:
                 min['score'] = temp_score
@@ -171,27 +165,28 @@ class Board():
 
     def proceed(self, player_row, player_col):
         start = time.time()
+        Board.leaf_num = 0
         if not self.can_put(player_row, player_col):
             return STATUS_CANNOT_PUT, None, None
 
-        self.put(player_row, player_col, self.player_color, record_history=True)
+        self.put(player_row, player_col, self.player_color, record=True)
 
-        score = self.evaluate(self.player_color)
+        score = self.evaluate()
         if score <= LOSS_THRESHOLD:
             return STATUS_PLAYER_WIN, None, None
 
-        score, ai_row, ai_col = self.max(4, MAX)
+        score, ai_row, ai_col = self.max(4, MIN, MAX)
 
         if score <= LOSS_THRESHOLD:
             return STATUS_PLAYER_WIN, None, None
 
-        self.put(ai_row, ai_col, self.ai_color, record_history=True)
+        self.put(ai_row, ai_col, self.ai_color, record=True)
 
         end = time.time()
-        score = self.evaluate(self.ai_color)
+        score = self.evaluate()
 
         print('time cost: %.1f s, score: %d' % (end - start, score))
-        print(Board.num)
+        print('leaf num: %d' % Board.leaf_num)
 
         if score >= WIN_THRESHOLD:
             return STATUS_AI_WIN, int(ai_row), int(ai_col)
